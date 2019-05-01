08a17b9687ef38180797bc57a66ada56399234b9
Ana Clara <anitacedroguima@gmail.com>
2019-04-30 21:59:35 -0300

    trabalho1

---
+++ app/src/main/java/br/com/tc1/trabalho1/RecebeListaDeObjetosActivity.java
@@ -0,0 +1,55 @@
+package br.com.tc1.trabalho1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecebeListaDeObjetosActivity extends AppCompatActivity {
+    TextView cargaHoraria;
+    TextView codigo;
+    TextView descricao;
+    TextView cargaHorariaRelogio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recebe_lista_de_objetos);
+
+        ArrayList<Disciplina> listaDisciplina = getIntent().getParcelableArrayListExtra("listaDisciplinas");
+
+        Log.i("stesda",listaDisciplina.get(0).getDescricao());
+
+        if(listaDisciplina != null){
+
+            Disciplina dc = listaDisciplina.get(1);
+
+            Log.i("carga",String.valueOf(dc.getCargaHoraria()));
+            Log.i("codigo",String.valueOf(dc.getCodigo()));
+            Log.i("desc",dc.getDescricao());
+            Log.i("carga2",String.valueOf(dc.convertCargaToHora()));
+
+            cargaHoraria = findViewById(R.id.cargaHorario);
+            codigo = findViewById(R.id.codigo);
+            descricao = findViewById(R.id.descricao);
+            cargaHorariaRelogio = findViewById(R.id.cargaHorarioRelogio);
+
+            String cargaHorariaPlace = "Carga Horária: "+ String.valueOf(dc.getCargaHoraria());
+            String codigoPlace = "Codigo: " + String.valueOf(dc.getCodigo());
+            String descricaoPlace = "Descrição: " + dc.getDescricao();
+            String cargaHorariaRelogioPlace = "Carga  Horária(converted): " + String.valueOf(dc.convertCargaToHora());
+
+            cargaHoraria.setText(cargaHorariaPlace);
+            codigo.setText(codigoPlace);
+            descricao.setText(descricaoPlace);
+            cargaHorariaRelogio.setText(cargaHorariaRelogioPlace);
+        }
+
+
+
+
+    }
+}
+
